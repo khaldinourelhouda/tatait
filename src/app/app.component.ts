ef9046e1b6c3be2292918fa0151f21983f2b3e77
nour2016 <khaldinourelhouda2016@gmail.com>
2020-08-24 09:57:13 +0100

    add comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   
 ngOnInit() {
 
+ /* hide navbar specifique (n2) de l'application et show navbar global (n1)  de l'application*/
   $("#n2").hide();
   $("#n1").show();
   
@@ -19,18 +20,20 @@ ngOnInit() {
 }
 
   constructor( public translate:TranslateService){
+    /* changement de la langue de l'application*/
     translate.addLangs(['en','fr']);
     translate.setDefaultLang('en');
     const browserLang =translate.getBrowserLang();
     translate.use(browserLang.match(/en|fr/) ? browserLang:'en');
 
-
+    
     localStorage.setItem("h1","true");
     localStorage.setItem("h2","false");
  }
 
  ch(){
     
+  /*cacher le navbar principale et afficher le navbar specipique de l'application */
   localStorage.setItem("h1","false");
 
   localStorage.setItem("h2","true");
